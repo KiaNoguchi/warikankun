af14c30ec2b117e6e855703a1c4223b4e208cf2c
KiaNoguchi <s1280094@u-aizu.ac.jp>
2022-05-30 13:28:39 +0900

    aiueo

---
+++ warikankun/warikankunApp.swift
@@ -0,0 +1,17 @@
+//
+//  warikankunApp.swift
+//  warikankun
+//
+//  Created by 野口季亜 on 2022/05/29.
+//
+
+import SwiftUI
+
+@main
+struct warikankunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
